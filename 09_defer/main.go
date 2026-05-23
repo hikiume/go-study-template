658d3d9a6daf41fb183c31dbd0edfa14669a6719
Hiki <hikicode@gmail.com>
2026-05-23 17:03:34 +0900

    [Add] sample code for defer

---
+++ 09_defer/main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"fmt"
+	"os"
+)
+
+
+func main(){
+	openFile()
+}
+
+func openFile(){
+	file, _ :=os.Open("./go.mod")
+	defer file.Close()
+	data :=make([]byte,100)
+	file.Read(data)
+	fmt.Println(string(data))
+}
+
+/*
+最初に書いたdefer文の処理が最後に実行される
+この場合だと
+run
+success
+3
+2
+1
+になる
+*/
+func sample(){
+	fmt.Println("run")
+	defer fmt.Println(1)
+	defer fmt.Println(2)
+	defer fmt.Println(3)
+	fmt.Println("success")
+}
+
+

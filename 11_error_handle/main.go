ed367309652970da7ded4691e6218b385ce62839
Hiki <hikicode@gmail.com>
2026-05-25 09:05:19 +0900

    [Add] sample code for error handle

---
+++ 11_error_handle/main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+)
+
+func main() {
+	file, err := os.Open("./lesson.go")
+	if err != nil {
+		log.Fatalln("Error!")
+	}
+
+	defer file.Close()
+	data := make([]byte, 100)
+
+	count, err := file.Read(data)
+	if err != nil {
+		log.Fatalln("Error")
+	}
+
+	fmt.Println(count, string(data))
+}

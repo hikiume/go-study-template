aa9ec0c297f9e165b2cb0b2a345fd421091457ae
Hiki <hikicode@gmail.com>
2026-05-27 14:01:10 +0900

    [Add] sample code for custom error

---
+++ 20_error/main.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+
+func myfunc() error {
+	ok := false
+	if ok {
+		return nil
+	}
+	return &UserNotFound{UserName: "mike"}
+}
+
+type UserNotFound struct {
+	UserName string
+}
+
+func (e *UserNotFound) Error() string {
+	return fmt.Sprintf("User not found: %v", e.UserName)
+}
+
+func main() {
+	e1 := &UserNotFound{"mike"}
+	e2 := &UserNotFound{"mike"}
+	fmt.Println(e1 == e2)
+	if err := myfunc(); err != nil {
+		fmt.Println(err)
+		if err == e1 {
+		}
+		if err == e2 {
+		}
+	}
+}

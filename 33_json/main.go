8d617bec9d037a3ec25b435f695d1dd650771264
Hiki <hikicode@gmail.com>
2026-05-31 14:51:24 +0900

    [Add] sample code for json

---
+++ 33_json/main.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+)
+
+type T struct{}
+
+type Person struct {
+	Name      string   `json:"name,omitempty"`
+	Age       int      `json:"age,omitempty"`
+	Nicknames []string `json:"nicknames,omitempty"`
+	T         *T       `json:"T,omitempty"`
+}
+
+// UnmarshalJSON を実装することでカスタマイズすることができる
+func (p *Person) UnmarshalJSON(b []byte) error {
+	type Person2 struct {
+		Name string
+	}
+	var p2 Person2
+	err := json.Unmarshal(b, &p2)
+	if err != nil {
+		println(err)
+	}
+	p.Name = p2.Name + "!"
+	return err
+}
+
+func main() {
+	b := []byte(`{"name":"Mike","age":20,"nicknames":[]}`)
+	var p Person
+	if err := json.Unmarshal(b, &p); err != nil {
+		fmt.Println(err)
+	}
+
+	fmt.Println(p.Name, p.Age, p.Nicknames)
+
+	// 構造体→ json
+	v, _ := json.Marshal(p)
+	fmt.Println(string(v))
+}

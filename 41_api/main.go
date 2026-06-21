5fe0bf7765559028dc9e32dfec10080639c16ffe
Hiki <hikicode@gmail.com>
2026-06-22 04:12:02 +0900

    feat: add jsonDecode to api

--- 41_api/main.go
+++ 41_api/main.go
@@ -1,11 +1,18 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
-	"io"
 	"log"
 	"net/http"
 )
+
+type Post struct {
+	UserID int    `json:"userId"`
+	ID     int    `josn:"id"`
+	Title  string `json:"title"`
+	Body   string `json:"body"`
+}
 
 func main() {
 	url := "https://jsonplaceholder.typicode.com/posts/1"
@@ -18,11 +25,12 @@ func main() {
 
 	fmt.Printf("Status Code: %d\n", resp.StatusCode)
 
-	body, err := io.ReadAll(resp.Body)
-	if err != nil {
-		log.Fatalf("ボディの読み込みに失敗しました")
+	var post Post
+
+	if err := json.NewDecoder(resp.Body).Decode(&post); err != nil {
+		log.Fatalf("デコード失敗: %v", err)
 	}
 
-	fmt.Printf("Respons Body:%s\n", string(body))
-
+	fmt.Printf("ID: %d\n", post.ID)
+	fmt.Printf("タイトル: %s\n", post.Title)
 }

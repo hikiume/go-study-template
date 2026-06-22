ad60cb7d0e3e8e27e0c6a8c8737fb8bbd04cc608
Hiki <hikicode@gmail.com>
2026-06-23 08:19:44 +0900

    feat: sample code for read json file

---
+++ 42_read_json_file/main.go
@@ -0,0 +1,26 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"os"
+)
+
+func main() {
+	file, err := os.ReadFile("example.json")
+
+	if err != nil {
+		log.Fatalln("ファイル読み込みエラー")
+	}
+
+	fmt.Println(string(file))
+
+	var value map[string]string
+	jsonErr := json.Unmarshal(file, &value)
+	if jsonErr != nil {
+		log.Fatalln("json Unmarshal")
+	}
+
+	fmt.Println(value["test"])
+}

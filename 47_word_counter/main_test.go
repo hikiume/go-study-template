ea8e75354e7dce64c6d210c3d040713a41112ba1
Hiki <hikicode@gmail.com>
2026-07-03 08:14:56 +0900

    test: add word counter

---
+++ 47_word_counter/main_test.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"maps"
+	"strings"
+	"testing"
+)
+
+func TestCountWord(t *testing.T) {
+	tests := []struct {
+		name   string
+		word   []string
+		expect map[string]int
+	}{
+		{name: "正常系", word: strings.Fields("go python go rust go python java"), expect: map[string]int{"go": 3, "python": 2, "rust": 1, "java": 1}},
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			got := countWord(tt.word)
+
+			if !maps.Equal(got, tt.expect) {
+				t.Errorf("%s の結果が一致しません\n期待値: %v\n実際の値: %v", tt.name, tt.expect, got)
+			}
+		})
+	}
+}

472807589f340d399fb0725aaef89f69d382143b
Hiki <hikicode@gmail.com>
2026-07-02 09:50:48 +0900

    refactor: extract into a function word counter

--- 47_word_counter/main.go
+++ 47_word_counter/main.go
@@ -9,14 +9,20 @@ func main() {
 	text := "go python go rust go python java"
 	words := strings.Fields(text)
 
+	counts := countWord(words)
+
+	fmt.Println("単語の集計")
+	for word, count := range counts {
+		fmt.Printf("%s: %d回\n", word, count)
+	}
+}
+
+func countWord(words []string) map[string]int {
 	counts := make(map[string]int)
 
 	for _, word := range words {
 		counts[word]++
 	}
 
-	fmt.Println("単語の集計")
-	for word, count := range counts {
-		fmt.Printf("%s: %d回\n", word, count)
-	}
+	return counts
 }

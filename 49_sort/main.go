82a0ccae5c09f47a90cdd0c7a9ed168daaa793fe
Hiki <hikicode@gmail.com>
2026-07-06 08:00:06 +0900

    feat: sample code desc sort to sort

--- 49_sort/main.go
+++ 49_sort/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"cmp"
 	"fmt"
 	"slices"
 )
@@ -13,4 +14,16 @@ func main() {
 	fruites := []string{"banana", "apple", "orrage", "grape"}
 	slices.Sort(fruites)
 	fmt.Println("文字列のソート後:", fruites)
+
+	descSort()
+}
+
+func descSort() {
+	numbers := []int{2, 5, 6, 3, 1, 4}
+
+	slices.SortFunc(numbers, func(a, b int) int {
+		return cmp.Compare(b, a)
+	})
+
+	fmt.Println("降順ソート後:", numbers)
 }

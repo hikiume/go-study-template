e56165c78a42c1ebbbf16ae569dc8e27475a57fd
Hiki <hikicode@gmail.com>
2026-07-08 07:51:38 +0900

    feat: sample code for multi sort

--- 50_struct_sort/main.go
+++ 50_struct_sort/main.go
@@ -23,4 +23,27 @@ func main() {
 	})
 
 	fmt.Println("年齢が低い順:", users)
+
+	multiSort()
+}
+
+// 複数の条件でソート
+func multiSort() {
+	users := []User{
+		{Name: "Taro", Age: 25},
+		{Name: "Hanako", Age: 20},
+		{Name: "Alice", Age: 25},
+	}
+
+	slices.SortFunc(users, func(a, b User) int {
+		result := cmp.Compare(a.Age, b.Age)
+
+		if result != 0 {
+			return result
+		}
+
+		return cmp.Compare(a.Name, b.Name)
+	})
+
+	fmt.Println(users)
 }
